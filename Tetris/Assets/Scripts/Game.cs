afad6b04f4d2008f615254c4654c39e62e035cb8
Jiayu <lujiayulu1998@gmail.com>
2019-09-01 14:37:21 +1000

    Upload

---
+++ Tetris/Assets/Scripts/Game.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+    public static int gridWidth = 10;
+    public static int gridHeight = 20;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnNextTetromino();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SpawnNextTetromino()
+    {
+        GameObject nextTetris = (GameObject)Instantiate(Resources.Load(GetRandomTetromino(), typeof(GameObject)), new Vector2(5.0f, 20.0f), Quaternion.identity);
+    }
+
+    public bool CheckIsInsideGrid(Vector2 pos)
+    {
+        return ((int)pos.x >= 0 && (int)pos.x < gridWidth && (int)pos.y >= 0);
+    }
+
+    public Vector2 Round (Vector2 pos)
+    {
+        return new Vector2(Mathf.Round(pos.x), Mathf.Round(pos.y));
+    }
+
+    string GetRandomTetromino()
+    {
+        int radomTetromino = Random.Range(1, 8);
+
+        string randomTetrominoName = "Prefabs/T";
+
+        switch (radomTetromino)
+        {
+            case 1:
+                randomTetrominoName = "Prefabs/T";
+                break;
+            case 2:
+                randomTetrominoName = "Prefabs/Long";
+                break;
+            case 3:
+                randomTetrominoName = "Prefabs/Square";
+                break;
+            case 4:
+                randomTetrominoName = "Prefabs/J";
+                break;
+            case 5:
+                randomTetrominoName = "Prefabs/S";
+                break;
+            case 6:
+                randomTetrominoName = "Prefabs/Z";
+                break;
+            case 7:
+                randomTetrominoName = "Prefabs/L";
+                break;
+        }
+
+        return randomTetrominoName;
+    }
+}
---
+++ Tetris/Assets/Scripts/Tetris.cs
@@ -0,0 +1,132 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tetris : MonoBehaviour
+{
+
+    float fall = 0;
+    public float fallSpeed = 1;
+
+    public bool allowRotation = true;
+    public bool limitRotation = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckUserInput();
+    }
+
+    void CheckUserInput()
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
+        {
+            transform.position += new Vector3(1, 0, 0);
+
+            if (CheckIsValidPosition())
+            {
+
+            }
+            else
+            {
+                transform.position += new Vector3(-1, 0, 0);
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
+        {
+            transform.position += new Vector3(-1, 0, 0);
+
+            if (CheckIsValidPosition())
+            {
+
+            }
+            else
+            {
+                transform.position += new Vector3(1, 0, 0);
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W))
+        {
+            if (allowRotation)
+            {
+                if (limitRotation)
+                {
+                    if (transform.rotation.eulerAngles.z >= 90)
+                    {
+                        transform.Rotate(0, 0, -90);
+                    }
+                    else
+                    {
+                        transform.Rotate(0, 0, 90);
+                    }
+                }
+                else
+                {
+                    transform.Rotate(0, 0, 90);
+
+                    if (CheckIsValidPosition())
+                    {
+
+                    }
+                    else
+                    {
+                        if (limitRotation)
+                        {
+                            if (transform.rotation.eulerAngles.z >= 90)
+                            {
+                                transform.Rotate(0, 0, -90);
+                            }
+                            else
+                            {
+                                transform.Rotate(0, 0, -90);
+                            }
+                        }
+                        else
+                        {
+                            transform.Rotate(0, 0, -90);
+                        }
+                    }
+                }
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S) || Time.time - fall >= fallSpeed)
+        {
+            transform.position += new Vector3(0, -1, 0);
+
+            if (CheckIsValidPosition())
+            {
+
+            }
+            else
+            {
+                transform.position += new Vector3(0, 1, 0);
+
+                enabled = false;
+
+                FindObjectOfType<Game>().SpawnNextTetromino();
+            }
+
+            fall = Time.time;
+        }
+    }
+
+    bool CheckIsValidPosition()
+    {
+        foreach (Transform mino in transform)
+        {
+            Vector2 pos = FindObjectOfType<Game>().Round(mino.position);
+
+            if (FindObjectOfType<Game>().CheckIsInsideGrid(pos) == false)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
